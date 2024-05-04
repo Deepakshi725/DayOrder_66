fb68f401999f54322a246dc768ed3dfe090892c9
Deepakshi275 <sharmapanchi99@gmail.com>
2024-05-04 16:30:04 +0530

    first commit

---
+++ pronote/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Editor from './Component/Editor'; // Import the Editor component
+
+function App() {
+  return (
+    <div className="App">
+      <Editor /> {/* Render the Editor component */}
+    </div>
+  );
+}
+
+export default App;
